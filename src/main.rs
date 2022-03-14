d3d7af4b4e59a57d688b6341806b8940f1eb682b
Paulo Alexandre Mello <palexandremello@gmail.com>
2022-03-14 20:56:44 -0300

    global variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+static GLOBAL_VARIABLE:u8 = 1;
+
 fn main() {    
     let variavel:i32 = 128;
     println!("variavel = {}, tamanho = {}", variavel, std::mem::size_of_val(&variavel));
@@ -15,4 +17,5 @@ fn main() {
 
     const PI:f32 = 3.14;
     println!("PI = {}", PI);
+    println!("Global variable = {}", GLOBAL_VARIABLE);
 }
