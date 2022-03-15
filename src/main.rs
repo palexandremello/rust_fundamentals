598125be60ba4963a9f4200409677f752e311215
Paulo Alexandre Mello <palexandremello@gmail.com>
2022-03-14 21:22:16 -0300

    scope

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 static GLOBAL_VARIABLE:u8 = 1;
 
+fn sombra () {
+    let a = 123;
+    {
+        let b = 456;
+        println!("address b {:p}", &b);
+        println!("address a {:p}", &a);
+
+    }
+    println!("address a {:p}", &a);
+}
 
 fn escopo() {
     let variavel:i32 = 300;
-    println!("variavel = {}, tamanho = {:p}", variavel, &variavel);
+    println!("variavel = {}, address = {:p}", variavel, &variavel);
     let variavel:i32 = 301;
-    println!("variavel = {}, tamanho = {:p}", variavel, &variavel);
+    println!("variavel = {}, address = {:p}", variavel, &variavel);
     let decimal:f32 = 2.5;
     println!("{}", decimal);
 
@@ -21,6 +31,7 @@ fn escopo() {
     println!("PI = {}", PI);
     println!("Global variable = {}", GLOBAL_VARIABLE);
 }
-fn main() {    
-    escopo()
+fn main() { 
+    sombra();  
+    escopo();
 }
