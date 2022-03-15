5ba9402f2198c46159b27c67f6478a2ef1fb94ca
Paulo Alexandre Mello <palexandremello@gmail.com>
2022-03-14 21:36:15 -0300

    if statements

--- src/if_statements.rs
+++ src/if_statements.rs
@@ -2,8 +2,9 @@
 
 fn main() {
     let age: u8 = 14;
+    let is_root:bool = true;
 
-    if age > 18 {
+    if age > 18 || age >= 14 && is_root {
         println!("Ok");
     } else {
         println!("not okay");
