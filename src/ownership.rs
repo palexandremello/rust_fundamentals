4a69c42436995bb3858d66d575365a5f2e2a113f
Paulo Alexandre Mello <palexandremello@gmail.com>
2022-03-16 22:35:28 -0300

    pattern matching

--- src/ownership.rs
+++ src/ownership.rs
@@ -1,14 +1,14 @@
 
 
 fn ownership() {
-    let one_string = String::from("Stringzinha");
-    rouba(&one_string);
+    let mut one_string = String::from("Stringzinha");
+    rouba(&mut one_string);
 
     println!("{}", one_string);
 
 }
 
-fn rouba(string: &String) {
+fn rouba(string: &mut String) {
     println!("{}", string);
 }
 
